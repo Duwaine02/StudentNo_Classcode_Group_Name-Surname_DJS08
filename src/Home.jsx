54bef116a1832c6d4e12586311a4eac92d3c1a24
Duwaine02 <duwainewaine02@gmail.com>
2025-06-14 12:44:05 +0200

    I change the button tpo book now

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,7 +3,7 @@ export default function Home() {
     <div className="home-container">
       <h1>You got the travel plans, we got the travel vans.</h1>
       <p>Add adventure to your life by joining the #vanlife movement. Rent the perfect van to make your perfect road trip.</p>
-      <button className="find-van-btn">Find your van</button>
+      <button className="find-van-btn">Book now</button>
     </div>
   )
 }
